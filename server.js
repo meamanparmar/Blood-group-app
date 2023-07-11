a8b0871c7ad7ca2104745736ab21ceeaf48d0c35
meamanparmar <parmaraman9171@gmail.com>
2023-07-11 11:51:08 +0530

    first commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const colors = require("colors");
 const morgan = require("morgan");
 const cors = require("cors");
 const connectDB = require("./config/db");
-const path = require("path");
+// const path = require("path");
 //dot config
 dotenv.config();
 
@@ -27,10 +27,10 @@ app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
 app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
 app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
-app.use(express.static(path.join(__dirname, "./client/build")));
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// app.use(express.static(path.join(__dirname, "./client/build")));
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 //port
 const PORT = process.env.PORT || 9000;
 
